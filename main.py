8c695a10cb877e174d37c554ecdd2c6c6d85790b
Leonard Eshun <leonard@Leonards-MBP-3.lan>
2024-09-06 18:37:45 -0400

    Added the capital city finding functionality

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
+import json
+
+
 def add(a, b):
     return a + b
 
 
-def do_something():
-    print("Hello what's your name?")
+def get_the_capital_of_a_country(country: str) -> str:
+    capital = ""
+    # Open and read the JSON file
+    with open("country_capital.json", "r") as file:
+        data = json.load(file)
+        capital = (
+            data[country.lower()]
+            if data.get(country.lower(), "") != ""
+            else "The country you specified was not found!"
+        )
+    return capital
 
 
 if __name__ == "__main__":
     print(add(1, 2))
+    print(get_the_capital_of_a_country("Ghana"))
--- test_main.py
+++ test_main.py
@@ -1,20 +1,25 @@
 from main import add
+from main import get_the_capital_of_a_country
 
 
 def test_add():
     """Tewting the add function"""
     assert add(2, 2) == 4
     assert add(3, 2) == 5
-    print("hello")
 
 
-def test_add2():
-    """testing out add function"""
-    assert add(2, 2) == 4
-    assert add(3, 2) == 5
-    assert add(1, 2) == 3
+def test_countries():
+    """testing out get_the_capital_of_a_country function"""
+    assert get_the_capital_of_a_country("United States") == "Washington D.C."
+    assert get_the_capital_of_a_country("Ghana") == "Accra"
+    assert get_the_capital_of_a_country("united states") == "Washington D.C."
+    assert (
+        get_the_capital_of_a_country("London")
+        == "The country you specified was not found!"
+    )
 
 
 if __name__ == "__main__":
     test_add()
-    test_add2()
+    test_countries()
+    print("Test completed successfully")
